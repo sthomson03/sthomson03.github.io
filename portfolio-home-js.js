3298ee8b3ee67d47a35b16bf7ce9084d553ef670
Sam Thomson <33004011+sthomson03@users.noreply.github.com>
2023-06-19 22:59:49 +0100

    V2.0

--- portfolio-home-js.js
+++ portfolio-home-js.js
@@ -1,4 +1,9 @@
-function darkModeButton() {
+function darkModeButton(event) {
+    event.preventDefault(); // Prevent the default scroll behavior
     var element = document.body;
     element.classList.toggle("dark-mode");
+
+
+    var darkModeButton = document.querySelector(".dark-mode-button");
+    darkModeButton.classList.toggle("active");
 }
