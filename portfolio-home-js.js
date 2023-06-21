9da97d24d2dfc9747ad0cac4d1be4514680f8d55
Sam Thomson <33004011+sthomson03@users.noreply.github.com>
2023-06-21 22:26:52 +0100

    Add files via upload

--- portfolio-home-js.js
+++ portfolio-home-js.js
@@ -3,7 +3,18 @@ function darkModeButton(event) {
     var element = document.body;
     element.classList.toggle("dark-mode");
 
-
     var darkModeButton = document.querySelector(".dark-mode-button");
     darkModeButton.classList.toggle("active");
 }
+
+// Smooth scrolling for touchscreen devices
+if ("ontouchstart" in document.documentElement) {
+    const links = document.querySelectorAll(".navbar a");
+    links.forEach((link) => {
+      link.addEventListener("click", (event) => {
+        event.preventDefault();
+        const target = document.querySelector(link.getAttribute("href"));
+        target.scrollIntoView({ behavior: "smooth" });
+      });
+    });
+  }
