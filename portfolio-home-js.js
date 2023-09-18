1f44ac9623c564b1fff8a4f5a40cc401415154ed
Sam Thomson <33004011+sthomson03@users.noreply.github.com>
2023-09-18 17:21:35 +0100

    Projects now display differently on desktop/mobile

--- portfolio-home-js.js
+++ portfolio-home-js.js
@@ -11,4 +11,30 @@ document.addEventListener("DOMContentLoaded", function() {
     var darkModeButton = document.querySelector(".dark-mode-button");
     darkModeButton.addEventListener("click", toggleDarkMode);
     darkModeButton.addEventListener("touchstart", toggleDarkMode);
+
+    var projectsMobile = document.querySelectorAll(".projects-mobile-container .project");
+    var currentIndex = 0;
+
+    function showProject(index) {
+        projectsMobile.forEach(function(project, i) {
+            if (i === index) {
+                project.style.display = "block";
+            } else {
+                project.style.display = "none";
+            }
+        });
+    }
+
+    var prevButton = document.querySelector(".prev-project");
+    var nextButton = document.querySelector(".next-project");
+
+    prevButton.addEventListener("click", function() {
+        currentIndex = (currentIndex - 1 + projectsMobile.length) % projectsMobile.length;
+        showProject(currentIndex);
+    });
+
+    nextButton.addEventListener("click", function() {
+        currentIndex = (currentIndex + 1) % projectsMobile.length;
+        showProject(currentIndex);
+    });
 });
